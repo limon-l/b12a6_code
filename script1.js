fac36b1bdd51d45d43246dfa60be993f07ea08ff
limon-l <limonroyapu.edu@gmail.com>
2025-09-09 22:21:20 +0600

    reviewed again

--- script1.js
+++ script1.js
@@ -45,8 +45,8 @@ const displayCats = (trees) => {
 
     const treeCard = document.createElement("div");
     treeCard.innerHTML = `
-      <div class="rounded-lg bg-white p-4 md:w-[250px] w-[300px] md:mb-0 mb-4 justify-between">
-        <img class="md:w-[250px] w-[300px] h-[200px] rounded-lg md:mb-4 mb-2" src="${tree.image}" alt="">
+      <div class="rounded-lg bg-white p-4 md:w-[250px] w-[350px] md:mb-0 mb-4 justify-between">
+        <img class="md:w-[250px] w-[350px] h-[200px] rounded-lg md:mb-4 mb-2" src="${tree.image}" alt="">
         <h1  id="treesContainer" class="text-lg font-semibold md:mb-4 mb-2 inter-font cursor-pointer">${tree.name}</h1>
         <p class="text-sm text-gray-700 md:mb-4 mb-2">${matched.small_description}</p>
         <div class="flex justify-between md:mb-4 mb-2">
@@ -181,8 +181,8 @@ document
 
     const treeName = el.textContent;
 
-    const res = await fetch("https://openapi.programming-hero.com/api/plants");
-    const data = await res.json();
+    const res = fetch("https://openapi.programming-hero.com/api/plants");
+    const data = res.json();
     const tree = data.plants.find((p) => p.name === treeName);
 
     if (!tree) return;
